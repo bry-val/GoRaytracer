60f2cef8317dcf4d4422212fbb3b2024b1b249e5
Bryan Valencia <94031627+bry-val@users.noreply.github.com>
2024-03-10 22:51:20 -0500

    Defined point and vector constructor methods
    Points and Vectors are both tuples under the hood with the w parameter defining the difference. The String method explicitly tells you which is which.
    
    First test for the point function does not actually work. I think it may have something to do with the way the fact that the point method returns a pointer to a tuple.... looking into this

--- goraytracer.go
+++ goraytracer.go
@@ -1,1 +1,32 @@
-package goraytracer
+package main
+
+import "fmt"
+
+const EPSILON float64 = 0.00001
+
+type Tuple struct {
+	x, y, z, w float64
+}
+
+func (t Tuple) String() string {
+	kind := "Vector"
+	if t.w == 1.0 {
+		kind = "Point"
+	}
+	return fmt.Sprintf("x: %f\ny: %f\nz: %f\nType: %s", t.x, t.y, t.z, kind)
+}
+
+func point(x, y, z float64) *Tuple {
+	return &Tuple{x: x, y: y, z: z, w: 1.0}
+}
+
+func vector(x, y, z float64) *Tuple {
+	return &Tuple{x: x, y: y, z: z, w: 0.0}
+}
+
+func main() {
+	fmt.Println(point(1, 2, 3))
+	fmt.Println(vector(3, 1, 3))
+	fmt.Println(vector(3, 1, 3))
+
+}
--- goraytracer_test.go
+++ goraytracer_test.go
@@ -1,1 +1,28 @@
-package goraytracer
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPoint(t *testing.T) {
+	var tests = []struct {
+		x, y, z float64
+		want    *Tuple
+	}{
+		{1, 2, 3, &Tuple{1.0, 2.0, 3.0, 1.0}},
+		{4, -3, 2, &Tuple{4.0, -3.0, 2.0, 1.0}},
+		{1, 2, 5, &Tuple{1.0, 2.0, 5.0, 1.0}},
+		{-1, 10, -3, &Tuple{-1.0, 10.0, -3.0, 1.0}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%f,%f,%f", tt.x, tt.y, tt.z)
+		t.Run(testname, func(t *testing.T) {
+			ans := point(tt.x, tt.y, tt.z)
+			if ans != tt.want {
+				t.Errorf("\n\n***GOT*** \n%s\n\n***WANT***\n%s", ans, tt.want)
+			}
+		})
+	}
+}
