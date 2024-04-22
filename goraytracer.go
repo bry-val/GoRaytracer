815c3285a6043ece007d34613016079122e4af72
Bryan Valencia <94031627+bry-val@users.noreply.github.com>
2024-04-22 08:56:10 -0500

    added test for operation functions

--- goraytracer.go
+++ goraytracer.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 const EPSILON float64 = 0.00001
 
@@ -9,13 +12,17 @@ type Tuple struct {
 }
 
 func (t Tuple) String() string {
-	kind := "Vector"
+	kind := "Invalid"
 	if t.w == 1.0 {
 		kind = "Point"
+	} else if t.w == 0.0 {
+		kind = "Vector"
 	}
 	return fmt.Sprintf("x: %f\ny: %f\nz: %f\nType: %s\n", t.x, t.y, t.z, kind)
 }
 
+// a tuple is a point when w is 1
+// a tuple is a vector when w is 0
 func point(x, y, z float64) Tuple {
 	return Tuple{x: x, y: y, z: z, w: 1.0}
 }
@@ -24,9 +31,32 @@ func vector(x, y, z float64) Tuple {
 	return Tuple{x: x, y: y, z: z, w: 0.0}
 }
 
+func add(a, b Tuple) Tuple {
+	return Tuple{a.x + b.x,
+		a.y + b.y,
+		a.z + b.z,
+		a.w + b.w,
+	}
+}
+func subtract(a, b Tuple) Tuple {
+	return Tuple{a.x - b.x,
+		a.y - b.y,
+		a.z - b.z,
+		a.w - b.w,
+	}
+}
+
+func equal(a, b Tuple) bool {
+	return math.Abs(a.x-b.x) < EPSILON &&
+		math.Abs(a.y-b.y) < EPSILON &&
+		math.Abs(a.z-b.z) < EPSILON &&
+		math.Abs(a.w-b.w) < EPSILON
+}
+
 func main() {
-	fmt.Println(point(1, 2, 3))
-	fmt.Println(vector(3, 1, 3))
-	fmt.Println(vector(3, 1, 3))
+	fmt.Println(subtract(point(1, 2, 3), point(1, 2, 3)))
+	fmt.Println(subtract(point(1, 2, 3), vector(1, 2, 3)))
+	fmt.Println(subtract(vector(1, 2, 3), vector(1, 2, 3)))
+	fmt.Println(subtract(vector(1, 3, 3), point(1, 2, 3)))
 
 }
--- goraytracer_test.go
+++ goraytracer_test.go
@@ -47,3 +47,67 @@ func TestVector(t *testing.T) {
 		})
 	}
 }
+func TestEqual(t *testing.T) {
+	var tests = []struct {
+		a, b Tuple
+		want bool
+	}{
+		{point(1, 2, 3), point(1, 2, 3), true},
+		{point(1.1, 2, 3.1), point(1.4, 2, 3.001), false},
+		//z component subtraction == 0.000049, test should return false
+		{vector(1.0000001, 2.0, 3.000001), vector(1.0, 2.000001, 3.00005), false},
+		{vector(1, 2, 3), point(1, 2, 3), false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := equal(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("\n\n***GOT*** \n%t\n\n***WANT***\n%t", ans, tt.want)
+			}
+		})
+	}
+}
+func TestAdd(t *testing.T) {
+	var tests = []struct {
+		a, b Tuple
+		want Tuple
+	}{
+		{point(1, 2, 3), point(1, 2, 3), Tuple{2, 4, 6, 2}},
+		{point(1, 2, 3), vector(1, 2, 3), Tuple{2, 4, 6, 1}},
+		{vector(1, 2, 3), point(1, 2, 3), Tuple{2, 4, 6, 1}},
+		{vector(1, 2, 3), vector(1, 2, 3), Tuple{2, 4, 6, 0}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := add(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("\n\n***GOT*** \n%s\n\n***WANT***\n%s", ans, tt.want)
+			}
+		})
+	}
+}
+func TestSubtract(t *testing.T) {
+	var tests = []struct {
+		a, b Tuple
+		want Tuple
+	}{
+		{point(1, 2, 3), point(1, 2, 3), Tuple{0, 0, 0, 0}},
+		{point(1, 2, 3), vector(1, 2, 3), Tuple{0, 0, 0, 1}},
+		{vector(1, 2, 3), point(1, 2, 3), Tuple{0, 0, 0, -1}},
+		{vector(1, 2, 3), vector(1, 2, 3), Tuple{0, 0, 0, 0}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := subtract(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("\n\n***GOT*** \n%s\n\n***WANT***\n%s", ans, tt.want)
+			}
+		})
+	}
+}
